0b87accbd78e32bdbd318c09cc333109f6f5b389
Larry Luk <larryluk526@gmail.com>
2017-11-14 11:08:57 -0500

    Initial Commit

---
+++ EveryDo/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  EveryDo
+//
+//  Created by Larry Luk on 2017-11-14.
+//  Copyright © 2017 Larry Luk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ EveryDo/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  EveryDo
+//
+//  Created by Larry Luk on 2017-11-14.
+//  Copyright © 2017 Larry Luk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
